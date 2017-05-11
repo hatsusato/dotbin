3974342ee0b871facfbe19da571c119e67620bb2
hatsusato <knightspm5@gmail.com>
2017-05-12 02:18:05 +0900

    Add Error

--- utility.sh
+++ utility.sh
@@ -8,3 +8,22 @@ fi
 readonly BIN_DIR=$(realpath "$(dirname "${UTILITY_FILE}")")
 export PATH="/bin:/usr/bin"
 export LANG='C'
+
+Error() {
+    local -i err=$?
+    if ((err == 0)); then
+        err=1
+    fi
+    if (($# == 0)); then
+        cat - >&2
+    else
+        local name=$(basename "$0")
+        local empty=$(head -c ${#name} </dev/zero | tr '\0' ' ')
+        for msg in "$@"
+        do
+            echo "${name}: ${msg}"
+            name="${empty}"
+        done >&2
+    fi
+    exit ${err}
+}

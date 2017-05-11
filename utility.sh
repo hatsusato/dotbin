c2b186c5ac704b3597675775d3e3616fd74b8114
hatsusato <knightspm5@gmail.com>
2017-05-12 02:17:21 +0900

    Add BIN_DIR

--- utility.sh
+++ utility.sh
@@ -5,5 +5,6 @@ if test "${DEBUG+'bound'}"; then
     # '$DEBUG' is bound variable
     set -vx
 fi
+readonly BIN_DIR=$(realpath "$(dirname "${UTILITY_FILE}")")
 export PATH="/bin:/usr/bin"
 export LANG='C'
